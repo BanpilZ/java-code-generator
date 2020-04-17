735fa3056aa9ab78db5c6190340731dfc85c51ce
1008185X0 <zlw459@126.com>
2020-04-17 17:01:01 +0800

    add SpringBoot

---
+++ src/main/java/com/banpil/tool/CodeGeneratorApplication.java
@@ -0,0 +1,16 @@
+package com.banpil.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Banpil
+ * @Date 2020-4-9 11:12
+ **/
+@SpringBootApplication
+public class CodeGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeGeneratorApplication.class);
+    }
+}
